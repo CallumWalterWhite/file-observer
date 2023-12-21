035ccb0a0ce91ff60b7a3487e8823527edcd07d1
Callum White <callumwhi@live.co.uk>
2023-12-21 01:19:31 +0000

    chore(cleanup): data is requested from service

--- app/folder_app.py
+++ app/folder_app.py
@@ -1,28 +1,22 @@
 import json
-import shutil
 import tkinter as tk
 from tkinter import ttk
 import customtkinter
 from app.tcp_client import TCPClient
 from .components import show_custom_dialog
 import uuid
-from .model import TagPath, Logs, db
 import os
-from service.file_tag_mover import FileMover
-import socket
 from .config import SERVICE_PORT, SERVICE_HOST
 
 class FolderApp:
     def __init__(self, root):
         self.root = root
         self.setup_gui()
-        self.setup_database()
         self.setup_style()
         self.client = TCPClient(SERVICE_HOST, SERVICE_PORT)
         self.home_frame = tk.Frame(root, bg="#ecf0f1")
         self.tags_frame = tk.Frame(root, bg="#ecf0f1")
         self.history_frame = tk.Frame(root, bg="#ecf0f1")
-        self.refresh_w = True
         self.watches = []
         self.show_screen()
         
@@ -37,16 +31,24 @@ class FolderApp:
         finally:
             self.client.close_connection()
     
+    def _send_command_request(self, command):
+        request = {'command': command}
+        request_json = json.dumps(request)
+        try:
+            self.client.connect()
+            self.client.send_message(request_json)
+            response = self.client.receive_response()
+            print("Received response:", response)
+        finally:
+            self.client.close_connection()
+        return json.loads(response)
+    
     def setup_gui(self):
         self.root.title("Folder Sorter Menu")
         self.root.geometry("1280x960")
         self.root.option_add("*Font", "helvetica 12")
         self.root.option_add("*TButton*relief", "flat")
-
-    def setup_database(self):
-        db.connect()
-        db.create_tables([TagPath, Logs])
-
+        
     def setup_style(self):
         style = ttk.Style()
         style.theme_use("clam")
@@ -59,7 +61,6 @@ class FolderApp:
         customtkinter.CTkLabel(self.home_frame, text="Tag Screen", font=("Helvetica", 16)).pack()
         self.tags_frame.pack(fill=tk.BOTH, expand=True)
         self.create_tags_table()
-        self.refresh_watches()
         customtkinter.CTkLabel(self.home_frame, text="History Screen", font=("Helvetica", 16)).pack()
         self.history_frame.pack(fill=tk.BOTH, expand=True)
         self.create_history_table() 
@@ -71,7 +72,7 @@ class FolderApp:
         tree = ttk.Treeview(self.history_frame, columns=columns, show="headings", selectmode="browse", style="Treeview")
         self.setup_tree(tree, columns)
 
-        logs = Logs.select()
+        logs = self.get_list_logs()
         for log in logs:
             tree.insert("", "end", values=(log.text, log.timestamp))
 
@@ -82,9 +83,9 @@ class FolderApp:
         tree = ttk.Treeview(self.tags_frame, columns=columns, show="headings", selectmode="browse", style="Treeview")
         self.setup_tree(tree, columns)
 
-        self.tags = TagPath.select()
+        self.tags = self.get_list_tag_path()
         for row in self.tags:
-            tree.insert("", "end", values=(row.id, row.sourcepath, row.tags, row.targetpath))
+            tree.insert("", "end", values=(row['id'], row['sourcepath'], row['tags'], row['targetpath']))
 
         tree.pack(padx=10, pady=20, fill=tk.BOTH, expand=True)
         self.setup_buttons(tree)
@@ -107,31 +108,42 @@ class FolderApp:
         auto_tag = customtkinter.CTkButton(self.tags_frame, text="Auto Tag", command=lambda: self.auto_tag(tree))
         auto_tag.pack(side=tk.LEFT, padx=10)
         
-        auto_tag = customtkinter.CTkButton(self.tags_frame, text="Trigger Move", command=lambda: self.trigger_move(tree))
+        auto_tag = customtkinter.CTkButton(self.tags_frame, text="Trigger Move", command=lambda: self._trigger_move(tree))
         auto_tag.pack(side=tk.LEFT, padx=10)
         
-    def trigger_move(self, tree):
+    def _trigger_move(self, tree):
         selected_item = tree.selection()
         if selected_item:
             current_data = tree.item(selected_item, "values")
             self._send_command('trigger', {'source_path': current_data[1], 'tags': current_data[2], 'target_path': current_data[3]})
             
+    def _send_tag_path(self, tag_path, command):
+        request_body = {
+            "id": str(tag_path['id']),
+            "source_path": tag_path['sourcepath'], 
+            "tags": tag_path['tags'], 
+            "target_path": tag_path['targetpath']
+        }
+        self._send_command(command, request_body)
+            
     def auto_tag(self, tree):
         source_path = show_custom_dialog("Auto Tag", "Enter Source Path:", selectFolder=True)
         if source_path is None:
             return
-        target_path = show_custom_dialog("Auto Tag", "Enter Target path for tagging:")
+        target_path = show_custom_dialog("Auto Tag", "Enter Target path for tagging:", selectFolder=True)
         if target_path is None:
             return
         lowest_level_directories = [root for root, dirs, files in os.walk(target_path) if not dirs]
         for path in lowest_level_directories:
-            base = os.path.basename(os.path.normpath(path))
+            tag = os.path.basename(os.path.normpath(path))
             id = uuid.uuid4()
-            TagPath.create(id=id, sourcepath=source_path, tags=base, targetpath=path)
-            tree.insert("", "end", values=(id, source_path, base, path))
-        self.tags = TagPath.select()
-        self.refresh_w = True
-        self.refresh_watches()
+            self._send_command('add', {
+                "id": str(id),
+                "source_path": source_path, 
+                "tags": tag, 
+                "target_path": path
+            })
+            tree.insert("", "end", values=(id, source_path, tag, path))
 
     def add_row(self, tree):
         source_path = show_custom_dialog("Add Row", "Enter Source Path:", selectFolder=True)
@@ -144,23 +156,24 @@ class FolderApp:
         if target_path is None:
             return
         id = uuid.uuid4()
-        tag = TagPath.create(id=id, sourcepath=source_path, tags=tags, targetpath=target_path)
+        self._send_command('add', {
+            "id": str(id),
+            "source_path": source_path, 
+            "tags": tags, 
+            "target_path": target_path
+        })
         if source_path and tags and target_path:
             tree.insert("", "end", values=(id, source_path, tags, target_path))
-        self.tags = TagPath.select()
-        self.refresh_w = True
-        self.refresh_watches()
 
     def remove_row(self, tree):
         selected_item = tree.selection()
         current_data = tree.item(selected_item, "values")
         id = current_data[0]
-        TagPath.get(TagPath.id == id).delete_instance()
+        self._send_command('delete', {
+            "id": str(id)
+        })
         if selected_item:
             tree.delete(selected_item)
-        self.tags = TagPath.select()
-        self.refresh_w = True
-        self.refresh_watches()
 
     def edit_row(self, tree):
         selected_item = tree.selection()
@@ -178,16 +191,15 @@ class FolderApp:
                 return
             if source_path is not None and tags is not None and target_path is not None:
                 tree.item(selected_item, values=(current_data[0], source_path, tags, target_path))
-            TagPath.update(sourcepath=source_path, tags=tags, targetpath=target_path).where(TagPath.id == id)
-            self.tags = TagPath.select()
-            self.refresh_w = True
-            self.refresh_watches([current_data[0]])
-
-    def refresh_watches(self, refresh_ids=[]):
-        if self.refresh_w == True:
-            self._send_command('trigger', {'refresh_ids': refresh_ids})
-            self.refresh_w = False
-
-    def watcher_callback(self, logs):
-        Logs.create(text=logs)
-        self.create_history_table()
+            self._send_command('update', {
+                "id": str(id),
+                "source_path": source_path, 
+                "tags": tags, 
+                "target_path": target_path
+            })
+            
+    def get_list_tag_path(self):
+        return self._send_command_request('list')
+    
+    def get_list_logs(self):
+        return self._send_command_request('logs')
--- service/file_watcher_client.py
+++ service/file_watcher_client.py
@@ -3,9 +3,9 @@ from service.config import PORT
 from service.file_watcher import FileWatcher
 import socket
 import threading
-from multiprocessing import Process
 from service.model import TagPath, Logs, db
 from service.file_tag_mover import FileMover
+import uuid
 
 class FileWatcherClient:
     def __init__(self):
@@ -21,6 +21,7 @@ class FileWatcherClient:
     def _init_watchers(self):
         self._tag_paths = TagPath.select()
         self.refresh_w = True
+        print(self._tag_paths)
         for tag_path in self._tag_paths:
             self.add_watcher(tag_path.sourcepath, tag_path.tags, tag_path.targetpath, tag_path.id, self._log)
 
@@ -43,11 +44,12 @@ class FileWatcherClient:
         request_body = request.decode('utf-8')
         print(f"Received request: {request_body}")
         request_command = json.loads(request_body)
-        self._handle_client_command(request_command)
-        client_socket.send(''.encode('utf-8'))
+        result = self._handle_client_command(request_command)
+        client_socket.send(result.encode('utf-8'))
         client_socket.close()
         
     def _handle_client_command(self, request_command):
+        result = ''
         try:
             if request_command['command'] == 'trigger':
                 source_path = request_command['body']['source_path']
@@ -55,15 +57,61 @@ class FileWatcherClient:
                 target_path = request_command['body']['target_path']
                 file_mover = FileMover(source_path, tags, target_path)
                 file_mover.move_files_by_tags()
+            elif request_command['command'] == 'add':
+                id = uuid.UUID(request_command['body']['id'])
+                source_path = request_command['body']['source_path']
+                tags = request_command['body']['tags']
+                target_path = request_command['body']['target_path']
+                TagPath.create(id=id, sourcepath=source_path, tags=tags, targetpath=target_path)
+                self._tag_paths = TagPath.select()
+                self.refresh_w = True
+                self.refresh_watches()
+            elif request_command['command'] == 'update':
+                id = request_command['body']['id']
+                source_path = request_command['body']['source_path']
+                tags = request_command['body']['tags']
+                target_path = request_command['body']['target_path']
+                TagPath.update(sourcepath=source_path, tags=tags, targetpath=target_path).where(TagPath.id == id)
+                self._tag_paths = TagPath.select()
+                self.refresh_w = True
+                self.refresh_watches()
+            elif request_command['command'] == 'delete':
+                id = uuid.UUID(request_command['body']['id'])
+                TagPath.get(TagPath.id == id).delete_instance()
+                self._tag_paths = TagPath.select()
+                self.refresh_w = True
+                self.refresh_watches()
             elif request_command['command'] == 'refresh':
                 self._tag_paths = TagPath.select()
                 self.refresh_w = True
-                self.refresh_watches(request_command['body']['refresh_ids'])
+                self.refresh_watches()
+            elif request_command['command'] == 'list':
+                self._tag_paths = TagPath.select()
+                tag_path_result = []
+                for tag_path in self._tag_paths:
+                    tag_path_result.append({
+                        'id': str(tag_path.id),
+                        'sourcepath': tag_path.sourcepath,
+                        'tags': tag_path.tags,
+                        'targetpath': tag_path.targetpath
+                    })
+                result = json.dumps(tag_path_result)
+            elif request_command['command'] == 'logs':
+                logs = Logs.select()
+                logs_result = []
+                for log in logs:
+                    logs_result.append({
+                        'text': log.text,
+                        'timestamp': log.timestamp
+                    })
+                result = json.dumps(logs_result)
             else:
                 print('Unknown command sent')
         except Exception as error:  
             print(error) #ignore exception
             #TODO: return exception to tcp
+        print(result)
+        return result
 
     def add_watcher(self, source, tags, target, meta_id=None, callback=None):
         self._fileWatchers.append(FileWatcher(source, tags, target, meta_id, callback))
@@ -73,12 +121,12 @@ class FileWatcherClient:
             print('Refreshing watches')
             for tagpath in self._tag_paths:
                 id = tagpath.id
-                if len(list(filter(lambda x: x.meta_id == id, self.watches))) == 0:
-                    self.add_watcher(tagpath.sourcepath, tagpath.tags, tagpath.targetpath, tagpath.meta_id, callback=self.watcher_callback)
+                if len(list(filter(lambda x: x.meta_id == id, self._fileWatchers))) == 0:
+                    self.add_watcher(tagpath.sourcepath, tagpath.tags, tagpath.targetpath, str(tagpath.id), callback=self._log)
                     print('New watcher added')
-                elif id in refresh_ids and len(list(filter(lambda x: x.meta_id == id, self.watches))) == 1:
-                    list(filter(lambda x: x.meta_id == id, self.watches))[0].stop()
-                    self.add_watcher(tagpath.sourcepath, tagpath.tags, tagpath.targetpath, tagpath.meta_id, callback=self.watcher_callback)
+                elif id in refresh_ids and len(list(filter(lambda x: x.meta_id == id, self._fileWatchers))) == 1:
+                    list(filter(lambda x: x.meta_id == id, self._fileWatchers))[0].stop()
+                    self.add_watcher(tagpath.sourcepath, tagpath.tags, tagpath.targetpath, str(tagpath.id), callback=self._log)
                     print('Watcher updated')
             self.refresh_w = False
 
@@ -97,3 +145,7 @@ class FileWatcherClient:
     def _log(self, log):
         Logs.create(log=log)
         print(log)
+
+if __name__ == '__main__':
+    fwc = FileWatcherClient()
+    fwc.start()
